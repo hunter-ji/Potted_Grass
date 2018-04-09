73229a09013d97db290520659e785f906179a7cd
kuari <kuari@justmylife.cc>
2018-04-09 14:54:57 +0800

    empty

--- db/insertData.py
+++ db/insertData.py
@@ -6,7 +6,7 @@ import os
 db = sqlite3.connect("class.db",check_same_thread = False)
 
 for id in range(1,9):
-    sid = "1508093021" + str(id)
+    sid = "178093021" + str(id)
     sname = "同学" + str(id)
     db.execute("insert into students (sid, sname) values (?,?)",[sid, sname])
     db.commit()
