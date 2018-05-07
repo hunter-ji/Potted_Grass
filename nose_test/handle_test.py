40f28a9e74f0cf8ef6723ee7b86e08d6b83718cf
kuari <kuari@justmylife.cc>
2018-05-07 10:38:31 +0800

    rm nose_test

--- nose_test/handle_test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-import os
-
-lines = os.popen("nosetests -s test_work.py").readlines()
-for line in lines:
-    print()
--- nose_test/test_work.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-from work import heihei
-
-def test_work():
-    assert heihei(1) == 2 
-
-if __name__ == "__main__":
-    test_work()
